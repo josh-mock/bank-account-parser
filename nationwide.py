2c4da9590a27a9e1dacd6b56ad5c4b2412b25db2
jwmock88 <jwmock@proton.me>
2024-10-15 14:59:15 +0100

    added functions

--- nationwide.py
+++ nationwide.py
@@ -1,10 +1,18 @@
 import csv
 import chardet
+import json
 from dateutil import parser
 from pathlib import Path
+from tabulate import tabulate
 
+# Name of folder containing input CSVs
 INPUT_FILE_PATH = "inputs"
+
+# Location of the Nationwide Account name on the first line pf the CSV statements
 ACCOUNT_NAME_LOCATION = 1
+
+# Category file
+CATEGORIES = "categories.json"
 
 
 def get_input_files(input_file_path):
@@ -13,8 +21,9 @@ def get_input_files(input_file_path):
 
     # Check if the file path is a directory
     if not directory.is_dir():
-        exit(f"Error: The specified path '{input_file_path}' is not a valid directory.")
-    
+        exit(f"Error: The specified path '{
+             input_file_path}' is not a valid directory.")
+
     # Find all CSV files in directory
     else:
         csv_files = list(directory.glob('*csv'))
@@ -22,7 +31,7 @@ def get_input_files(input_file_path):
         # Check if there are any csv files
         if len(csv_files) >= 1:
             return csv_files
-        
+
         else:
             exit(f"No CSV files found in directory: '{directory}'.")
 
@@ -43,6 +52,7 @@ def find_headings(infile):
     for _ in range(3):
         next(infile)
 
+
 def get_transaction_date(row):
     # Get date from row
     date_string = row.get("Date")
@@ -53,14 +63,15 @@ def get_transaction_date(row):
     # Return in yyyy-mm-dd format
     return date_object.strftime("%Y-%m-%d")
 
-def get_transaction_ammount(row):
+
+def get_transaction_value(row):
     paid_in = row.get("Paid in")
     paid_out = row.get("Paid out")
 
     if paid_in:
         # Remove the currency symbol and return
         return float(paid_in[1:])
-    
+
     if paid_out:
         # Remove the currency symbol and make negative
         return -float(paid_out[1:])
@@ -69,9 +80,86 @@ def get_transaction_ammount(row):
 def get_transaction_description(row, account_name):
     return row.get("Transactions") if account_name == "Nationwide Credit" else row.get("Description")
 
+
+def load_categories_json(file):
+    with open(file, 'r') as file:
+        return json.load(file)
+
+
+def categorise_transaction(transaction_description, account_name, transaction_date, transaction_value):
+    categories = load_categories_json(CATEGORIES)
+
+    # Iterate through each category and its associated list of keywords
+    for category, keywords in categories.items():
+        # Check if any keyword matches the transaction string snd return category if match
+        if any(keyword.lower() in transaction_description.lower() for keyword in keywords):
+            return category
+        # If not, prompt the user to enter a category
+        else:
+            table = [["Account:", account_name], ["Date:", transaction_date], [
+                "Amount:", transaction_value], ["Description:", transaction_description]]
+            print(f"\nThe following transaction could not be categorised: \n")
+            # Print the transaction
+            print(tabulate(table))
+            print("\nSelect the category you would like to assign it to:\n")
+            # Display category options
+            for index, category in enumerate(categories, start=1):
+                print(f"{index}: {category}")
+
+                while True:
+                    try:
+                        # Prompt the user to input a category ID
+                        assigned = int(input("\nEnter category id: "))
+
+                        # Validate the input and check if it matches a category
+                        if 1 <= assigned <= len(categories):
+                            category = list(categories.keys())[assigned - 1]
+                            print(
+                                f"\nTransaction '{transaction_description}' assigned to category: {category}")
+
+                            while True:
+                                decision = input(
+                                    "Would you like to add this to categories for future use? (Y/N): ").upper()
+
+                                if decision == "Y":
+                                    # Add the transaction as a keyword under the selected category
+                                    if transaction_description not in categories[category]:
+                                        categories[category].append(
+                                            transaction_description)
+                                        print(
+                                            f"\nTransaction '{transaction_description}' added to the '{category}' category.")
+                                    else:
+                                        print(
+                                            f"\nTransaction '{transaction_description}' is already in the '{category}' category.")
+                                    break
+                                elif decision == "N":
+                                    print(
+                                        f"\nTransaction '{transaction_description}' not added to any category.")
+                                    break
+                                else:
+                                    print(
+                                        "\nInvalid input. Please enter 'Y' or 'N'.")
+                        else:
+                            print(
+                                "\nInvalid input. Please enter a valid category ID.\n")
+        save_updated_category_keywords_to_json_file(categories, CATEGORIES)
+        return category
+
+
+def save_updated_category_keywords_to_json_file(categories, categories_file):
+    with open(categories_file, 'w') as file:
+        # Pretty-print with indentation
+        json.dump(categories, file, indent=4)
+
+
+def get_transaction_type():
+    transaction_description, account_name, transaction_date, transaction_value
+
+
 def main():
     # Get files from the user
     input_files = get_input_files(INPUT_FILE_PATH)
+    # Create empty list of transactions 
     # Iterate over files
     for input in input_files:
         with open(input, "r", encoding=get_encoding(input)) as infile:
@@ -89,15 +177,18 @@ def main():
                 # Get the transaction date
                 transaction_date = get_transaction_date(row)
 
-                # Get the transaction value 
+                # Get the transaction value
                 transaction_value = get_transaction_value()
 
                 # Get the transaction description
                 transaction_description = get_transaction_description()
 
                 # Categorise the transaction
-                transaction_category = categorise_transaction(transaction_description)
+                transaction_category = categorise_transaction(
+                    transaction_description, account_name, transaction_date, transaction_value)
 
-                # Give the transaction a type 
+                # Give the transaction a type
+                transaction_type = get_transaction_type(transaction_category)
 
-                # Return a transaction dictionary and append to the list of transactions 
+                # Return a transaction dictionary and append to the list of transactions
+
